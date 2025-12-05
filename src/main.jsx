fc8d07aad670a9e56712a1146e316752b9cb3aa5
Anwer Hosen <nur.cnits@gmail.com>
2025-12-05 22:23:06 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Hello world</p>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
